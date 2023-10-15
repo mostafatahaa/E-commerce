822c02d8077a77a3b7de2b6ed13e13a510f37417
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-15 12:44:18 +0300

    add dashboard route and use middleware to auth from user

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Defined a route using GET method
-Route::get('/dashboard/index', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 
 
