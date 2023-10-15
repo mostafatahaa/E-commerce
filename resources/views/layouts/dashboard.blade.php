296c4ccde2274712eace8704a4c73338b8ed24aa
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-15 18:08:36 +0300

    add CSRF token to form

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -176,7 +176,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         <img src="{{ asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                        <a href="#" class="d-block">{{ Auth::user()->name }}
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button class="btn btn-sm btn-outline-primary" href="{{ route('logout') }}">Logout</button class="btn btn">
+                            </form>
+
+                        </a>
                     </div>
                 </div>
                 @endauth
