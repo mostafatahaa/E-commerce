e8a141f31eddff60dba190081f54e6c3405c8396
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-15 12:45:02 +0300

    add css and js links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
 
 <body class="font-sans antialiased">
@@ -34,6 +35,7 @@
             {{ $slot }}
         </main>
     </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
