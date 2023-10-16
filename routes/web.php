c891a02721f87983771f341f6a4d143ba9d51e68
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-16 13:43:39 +0300

    make code more clean

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
-
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
 
 
 
 require __DIR__ . '/auth.php';
+require __DIR__ . '/dashboard.php';
