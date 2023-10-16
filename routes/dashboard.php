8a752a6c4943c37f3f9b94915632ceda57f70086
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-16 13:43:23 +0300

    make route in dashboard

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Dashboard\CategoriesController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard/index', [DashboardController::class, 'index'])
+Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
