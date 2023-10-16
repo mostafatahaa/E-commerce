1623675ca7d9567cf989978c5bc681c6b05007ec
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-16 09:58:14 +0300

    create dashboard route file

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,1 +1,11 @@
 <?php
+
+use App\Http\Controllers\Dashboard\CategoriesController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/dashboard/index', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::resource('dashboard/categories', CategoriesController::class);
