a1c06d2236877b970a57fb41a8a4c80083661349
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-16 13:42:41 +0300

    create file to create new category

---
+++ resources/views/dashboard/categories/create.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Categories')
+
+
+
+@section('breadcrumb')
+@parent
+
+<li class="breedcrumb-item active">Categories Page</li>
+
+@endsection
+
+
+
+
+@section('content')
+
+<form action="{{ route('categories.store')}} " method="post">
+    @csrf
+    <div class="form-group">
+        <lable>Category Name</lable>
+        <input type="text" name="name" class="form-control">
+    </div>
+    <div class="form-group">
+        <lable>Category Parent</lable>
+        <select name="parent_id" class="form-control form-select">
+            <option value="">Primary Category</option>
+
+            @foreach($parents as $parent)
+            <option value="{{ $parent->parent_id }}">{{$parent->name}}</option>
+            @endforeach
+
+        </select>
+    </div>
+    <div class="form-group">
+        <lable>Descreption</lable>
+        <textarea name="description" class="form-control"></textarea>
+    </div>
+
+    <div class="form-group">
+        <lable>Image</lable>
+        <input type="file" name="image" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <lable for="">Status</lable>
+        <div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="status" value="active" checked>
+                <label class="form-check-label">
+                    Active
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="status" value="arhived">
+                <label class="form-check-label">
+                    Archived
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <button class="btn btn-primary" type="submit">Save</button>
+    </div>
+</form>
+
+@endsection
