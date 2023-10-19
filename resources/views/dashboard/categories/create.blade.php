126fc45cf310571b5cf95ff36d600d295a7f8011
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-19 13:36:07 +0300

    include form to create file

--- resources/views/dashboard/categories/create.blade.php
+++ resources/views/dashboard/categories/create.blade.php
@@ -16,54 +16,13 @@
 
 @section('content')
 
-<form action="{{ route('dashboard.categories.store')}} " method="post">
+<form action="{{ route('dashboard.categories.store')}} " method="post" enctype="multipart/form-data">
     @csrf
-    <div class="form-group">
-        <lable>Category Name</lable>
-        <input type="text" name="name" class="form-control">
-    </div>
 
-    <div class="form-group">
-        <lable for="">Category Parent</lable>
-        <select name="parent_id" class="form-control form-select">
-            <option value="">Primary Category</option>
-            @foreach($parents as $parent)
-            <option value="{{ $parent->id }}">{{$parent->name}}</option>
-            @endforeach
+    @include('dashboard.categories._form', [
+    'btn_lable' => 'Create'
+    ])
 
-        </select>
-    </div>
-    <div class="form-group">
-        <lable>Descreption</lable>
-        <textarea name="description" class="form-control"></textarea>
-    </div>
-
-    <div class="form-group">
-        <lable>Image</lable>
-        <input type="file" name="image" class="form-control">
-    </div>
-
-    <div class="form-group">
-        <lable for="">Status</lable>
-        <div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="status" value="active" checked>
-                <label class="form-check-label">
-                    Active
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="status" value="archived">
-                <label class="form-check-label">
-                    Archived
-                </label>
-            </div>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <button class="btn btn-primary" type="submit">Save</button>
-    </div>
 </form>
 
 @endsection
