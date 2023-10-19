3b6bb3e8a3d5162dee1d927baba34a11b07f6eb1
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-19 13:36:22 +0300

    include form to edit file

--- resources/views/dashboard/categories/edit.blade.php
+++ resources/views/dashboard/categories/edit.blade.php
@@ -16,55 +16,15 @@
 
 @section('content')
 
-<form action="{{ route('dashboard.categories.update', $category->id)}} " method="post">
+<form action="{{ route('dashboard.categories.update', $category->id)}} " method="post" enctype="multipart/form-data">
     @csrf
     @method('put')
-    <div class="form-group">
-        <lable>Category Name</lable>
-        <input type="text" name="name" class="form-control" value="{{$category->name}}">
-    </div>
-    <div class="form-group">
-        <lable>Category Parent</lable>
-        <select name="parent_id" class="form-control form-select">
-            <option value="">Primary Category</option>
 
-            @foreach($parents as $parent)
-            <option value="{{ $parent->id }}" @selected($category->parent_id == $parent->id)>{{$parent->name}}</option>
-            @endforeach
+    @include('dashboard.categories._form', [
+    'btn_lable' => 'Update'
 
-        </select>
-    </div>
-    <div class="form-group">
-        <lable>Descreption</lable>
-        <textarea name="description" class="form-control"> {{ $category->description }}</textarea>
-    </div>
+    ])
 
-    <div class="form-group">
-        <lable>Image</lable>
-        <input type="file" name="image" class="form-control">
-    </div>
-
-    <div class="form-group">
-        <lable for="">Status</lable>
-        <div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="status" value="active" @checked($category->status == 'active')>
-                <label class="form-check-label">
-                    Active
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="status" value="archived" @checked($category->status == 'archived')>
-                <label class="form-check-label">
-                    Archived
-                </label>
-            </div>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <button class="btn btn-primary" type="submit">Save</button>
-    </div>
 </form>
 
 @endsection
