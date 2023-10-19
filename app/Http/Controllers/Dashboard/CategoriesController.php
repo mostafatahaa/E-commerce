aa4811a3469520e7b5897eee91ac1dd8e06a5602
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-19 15:44:57 +0300

    create validation roules by using validate method

--- app/Http/Controllers/Dashboard/CategoriesController.php
+++ app/Http/Controllers/Dashboard/CategoriesController.php
@@ -49,6 +49,12 @@ class CategoriesController extends Controller
     public function store(Request $request)
     {
 
+        $request->validate([
+            'name'          => 'required|string|min:3|max:255',
+            'parent_id'     => ['int', 'exists:categories,id'],
+            'image'         => 'image|max:1048576|dimensions:min_width=100,min_height=100',
+            'status'        => 'in:active,archived'
+        ]);
 
         // Requset merge
         $request->merge([
