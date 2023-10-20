231a8a0459503056fde97fe2de27d7784ad8d4a7
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-20 21:39:23 +0300

    create custom rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('filter', function ($attribute, $value, $params) {
+            return !in_array(strtolower($value), $params);
+        }, 'This Value is Already exists!');
     }
 }
