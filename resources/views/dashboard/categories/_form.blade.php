44c7fd758f51e232f0982730aa827f5dbf7a98b9
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-20 21:40:11 +0300

    set alert in view

--- resources/views/dashboard/categories/_form.blade.php
+++ resources/views/dashboard/categories/_form.blade.php
@@ -1,6 +1,25 @@
+@if($errors->any())
+<div class="alert alert-danger">
+    <h3>Error Occurred!</h3>
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>{{$error}}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
 <div class="form-group">
     <lable>Category Name</lable>
-    <input type="text" name="name" class="form-control" value="{{$category->name}}">
+    <input type="text" name="name" @class([ 'form-control' , 'is-invalid'=> $errors->has('name')
+    ])
+    value="{{old('name', $category->name)}}">
+
+
+    @error('name')
+    <div class="text-danger">{{$message}}</div>
+    @enderror
+
 </div>
 <div class="form-group">
     <lable>Category Parent</lable>
@@ -8,14 +27,20 @@
         <option value="">Primary Category</option>
 
         @foreach($parents as $parent)
-        <option value="{{ $parent->id }}" @selected($category->parent_id == $parent->id)>{{$parent->name}}</option>
+        <option value="{{ $parent->id }}" @selected(old('parent_id', $category->parent_id)==$parent->id)>{{$parent->name}}</option>
         @endforeach
 
     </select>
+    @error('parent_id')
+    <div class="text-danger">{{$message}}</div>
+    @enderror
 </div>
 <div class="form-group">
-    <lable>Descreption</lable>
-    <textarea name="description" class="form-control"> {{ $category->description }}</textarea>
+    <lable>Description</lable>
+    <textarea name="description" class="form-control"> {{ old('description', $category->description) }}</textarea>
+    @error('description')
+    <div class="text-danger">{{$message}}</div>
+    @enderror
 </div>
 
 <div class="form-group">
@@ -24,24 +49,30 @@
     @if($category->image)
     <img src="{{ asset('storage/' . $category->image) }}" height="100" alt="">
     @endif
+    @error('image')
+    <div class="text-danger">{{$message}}</div>
+    @enderror
 </div>
 
 <div class="form-group">
     <lable for="">Status</lable>
     <div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="status" value="active" @checked($category->status == 'active')>
+            <input class="form-check-input" type="radio" name="status" value="active" @checked(old('status', $category->status)=='active' )>
             <label class="form-check-label">
                 Active
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" type="radio" name="status" value="archived" @checked($category->status == 'archived')>
+            <input class="form-check-input" type="radio" name="status" value="archived" @checked(old('status', $category->status)=='archived' )>
             <label class="form-check-label">
                 Archived
             </label>
         </div>
     </div>
+    @error('status')
+    <div class="text-danger">{{$message}}</div>
+    @enderror
 </div>
 
 <div class="form-group">
