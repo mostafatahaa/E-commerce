399b1e8955a9d911d93004e7b16f4f13a48c55ec
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-20 21:38:23 +0300

    create method for rules

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Rules\Filter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class Category extends Model
 {
@@ -12,4 +14,25 @@ class Category extends Model
     protected $fillable = [
         'name', 'description', 'image', 'status', 'slug', 'parent_id'
     ];
+
+    public static function rules($id = 0)
+    {
+        return [
+            'name'          => [
+
+                'required',
+                'string',
+                'min:3',
+                'max:255',
+                // or "unique:categoreis,name,$id",
+                Rule::unique('categories', 'name')->ignore($id),
+                'filter:php,laravel,admin',
+
+                // new Filter(['MOstafa', 'admin', 'php']),
+            ],
+            'parent_id'     => ['nullable', 'int', 'exists:categories,id'],
+            'image'         => 'image|max:1048576|dimensions:min_width=100,min_height=100',
+            'status'        => 'in:active,archived'
+        ];
+    }
 }
