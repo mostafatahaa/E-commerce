a798e7232c42cffb761bc8d390fbb45019c3e5e1
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-21 18:03:58 +0300

    create input component

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,23 @@
+@props([
+
+'type' => 'text', 'value' => '', 'name', 'label' => false
+
+])
+
+@if($label)
+<lable>{{$label}}</lable>
+@endif
+
+<input label="Category Name" type="{{$type}}" name="{{$name}}" value="{{old($name, $value)}}" {{$attributes ->class([
+    'form-control',
+    'is-invalid' => $errors->has($name)
+])}}>
+
+@error($name)
+<div class="text-danger">{{$message}}</div>
+@enderror
+
+
+
+<!-- @class([ 'form-control' , 'is-invalid'=> $errors->has($name)
+]) -->
