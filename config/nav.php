5a0c74374604a0b2f8d5908e4191727c4d3e5e80
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-21 18:02:54 +0300

    create nav config file

---
+++ config/nav.php
@@ -0,0 +1,30 @@
+<?php
+
+
+return [
+    [
+        'icon'      => 'nav-icon fas fa-tachometer-alt',
+        'route'     => 'dashboard.dashboard',
+        'title'     => 'Dashboard',
+        'badge'     => 'New',
+        'active'    => 'dashboard.dashboard'
+    ],
+    [
+        'icon'      => 'far fa-circle nav-icont',
+        'route'     => 'dashboard.categories.index',
+        'title'     => 'Categories',
+        'active'    => 'dashboard.categories.*'
+    ],
+    [
+        'icon'      => 'far fa-circle nav-icont',
+        'route'     => 'dashboard.categories.index',
+        'title'     => 'Products',
+        'active'    => 'dashboard.products.*'
+    ],
+    [
+        'icon'      => 'far fa-circle nav-icont',
+        'route'     => 'dashboard.categories.index',
+        'title'     => 'Orders',
+        'active'    => 'dashboard.orders.*'
+    ],
+];
