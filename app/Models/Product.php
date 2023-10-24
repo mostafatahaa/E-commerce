15431c10894563641e82d12afdf65f5175fed206
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-24 22:53:25 +0300

    create product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,28 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\StoreScope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Product extends Model
 {
     use HasFactory;
 
-    // You will not need to use this property if you commit with laravel standard
-
-    // TIMESTAMP COLUMNS and you can change this defalt name
-    const CREATED_AT = 'created_at';
-    const UPDATED_AT = 'updated_at';
-
-    protected $connection = 'mysql';
-    protected $table = 'stores';
-    protected $primaryKey = 'id'; // auto_increment by default in laravel 
-
-    // to change auto_increment in laravel (true is default)
-    // this and $timestamp the only property that will be public
-    public $incrementing = true;
-
-    // Laravel always suppose that ur table have timestamps() even if you deleted it 
-    // so you can do set this
-    public $timestamps = true;
+    protected static function booted()
+    {
+        static::addGlobalScope('store', new StoreScope());
+    }
 }
