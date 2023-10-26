3d65f7f2adf6c940eff190c512becff242fba080
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-26 03:54:41 +0300

    set index action

--- app/Http/Controllers/Dashboard/ProductsController.php
+++ app/Http/Controllers/Dashboard/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,8 +18,9 @@ class ProductsController extends Controller
     public function index()
     {
 
-        $products = Product::paginate();
+        $products = Product::with(['category', 'store'])->paginate();
 
+        dd($products);
 
         return view('dashboard.products.index', compact('products'));
     }
