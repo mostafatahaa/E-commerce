3a47b34270ec11dafb1a8a589acacb9fac373d2d
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-26 03:55:13 +0300

    create relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,4 +16,14 @@ class Product extends Model
     {
         static::addGlobalScope('store', new StoreScope());
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class, 'store_id', 'id');
+    }
 }
