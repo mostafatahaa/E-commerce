966a61a9115fe146473f2ceb1708953d1cd0f893
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-26 03:58:09 +0300

    set category name by useing relation

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -53,8 +53,8 @@
         <tr>
             <td>{{$product->id}}</td>
             <td>{{$product->name}}</td>
-            <td>{{$product->category_id}}</td>
-            <td>{{$product->store_id}}</td>
+            <td>{{$product->category()->first()->name}}</td>
+            <td>{{$product->store->name}}</td>
             <td>{{$product->status}}</td>
             <td>{{$product->created_at}}</td>
             <td>
