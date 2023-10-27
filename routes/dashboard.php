94918e7e4936bf03c152218eba50caec713eadb2
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-27 16:14:06 +0200

    add profile routes

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Dashboard\CategoriesController;
 use App\Http\Controllers\Dashboard\ProductsController;
+use App\Http\Controllers\Dashboard\ProfileController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,19 +15,20 @@ Route::group([
     'prefix' => 'dashboard' // all routes  will start with dashboard 
 ], function () {
 
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
     Route::get('/', [DashboardController::class, 'index'])
         ->name('dashboard');
-
 
     Route::get('/categories/trash', [CategoriesController::class, 'trash'])
         ->name('categories.trash');
 
-    Route::put('categories/{category}/restore', [CategoriesController::class, 'restore'])
+    Route::put('/categories/{category}/restore', [CategoriesController::class, 'restore'])
         ->name('categories.restore');
 
-    Route::delete('categories/{category}/force-delete', [CategoriesController::class, 'forceDelete'])
+    Route::delete('/categories/{category}/force-delete', [CategoriesController::class, 'forceDelete'])
         ->name('categories.force-delete');
-
 
     Route::resource('/categories', CategoriesController::class);
     Route::resource('/products', ProductsController::class);
