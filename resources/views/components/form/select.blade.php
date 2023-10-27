3815a8b6d81fc586edd771d463e7ace01b89f216
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-27 16:13:14 +0200

    create component select

---
+++ resources/views/components/form/select.blade.php
@@ -0,0 +1,15 @@
+@props(['name', 'options', 'selected', 'label' => false])
+
+@if($label)
+<lable>{{$label}}</lable>
+@endif
+
+<select name="{{$name}}" {{$attributes->class([
+    'form-control',
+    'form-select',
+    'is-invalid' => $errors->has($name)
+    ])}}>
+    @foreach($options as $value => $text)
+    <option value="{{$value}}" @selected($value==$selected )>{{$text}}</option>
+    @endforeach
+</select>
