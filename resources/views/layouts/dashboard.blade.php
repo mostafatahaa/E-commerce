396fd11efc498dab71c95c47640c08afb5cfd377
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-28 19:12:59 +0200

    add @stack directive for css and js files

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -16,6 +16,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+    @stack('style')
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -266,6 +267,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <!-- AdminLTE App -->
     <script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+    @stack('script')
 </body>
 
 </html>
