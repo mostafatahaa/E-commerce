eee3fb07aa453ceb0b0c09928e07ca9aa45c8e86
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-28 19:09:08 +0200

    create update action

--- app/Http/Controllers/Dashboard/ProductsController.php
+++ app/Http/Controllers/Dashboard/ProductsController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
 
 class ProductsController extends Controller
 {
@@ -65,6 +68,10 @@ class ProductsController extends Controller
     {
 
         $product = Product::findOrFail($id);
+        $tags = implode(',', $product->tags()->pluck('name')->toArray());
+
+
+        return view('dashboard.products.edit', compact('product', 'tags'));
     }
 
 
@@ -75,9 +82,31 @@ class ProductsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->except('tags'));
+
+        $tags = json_decode($request->post('tags'));
+        $tag_ids = [];
+
+        $saved_tags = Tag::all();
+        foreach ($tags as $item) {
+            $slug = Str::slug($item->value);
+            $tag = $saved_tags->where('slug', $slug)->first();
+            if (!$tag) {
+                $tag = Tag::create([
+                    'name' => $item->value,
+                    'slug' => $slug
+                ]);
+            }
+
+            $tag_ids[] = $tag->id;
+        }
+
+        $product->tags()->sync($tag_ids);
+
+        return redirect()->route('dashboard.products.index')
+            ->with('success', 'Product Updated!');
     }
 
     /**
