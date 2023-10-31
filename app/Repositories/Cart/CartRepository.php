6047b83de6464df153575fd2772c2a791a787c1a
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-31 02:33:12 +0200

    create interface for cart using repository pattern

---
+++ app/Repositories/Cart/CartRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories\Cart;
+
+use App\Models\Product;
+use Illuminate\Support\Collection;
+
+interface CartRepository
+{
+    // get data from cart
+    public function get(): Collection; // this means must return collection
+
+    // add item to cart and its quantity...default = 1
+    public function add(Product $product, $quantity = 1);
+
+    // update quantity for item
+    public function update(Product $product, $quantity);
+
+    // delete specific item form cart
+    public function delete($id);
+
+    // clear all items form cart
+    public function empty();
+
+    // count all items in cart and return float
+    public function total(): float;
+}
