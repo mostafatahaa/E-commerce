00587f033eebbb685ceb30cf9afbb0c1bae56dd1
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-31 23:56:54 +0200

    create global scope for cookie_id and add method to get cookie id

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use App\Observers\CartObserver;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Str;
 
 class Cart extends Model
@@ -20,6 +22,10 @@ class Cart extends Model
     public static function booted()
     {
         static::observe(CartObserver::class);
+
+        static::addGlobalScope('cookie_id', function (Builder $builder) {
+            $builder->where('cookie_id', '=', self::getCookieId());
+        });
         // static::creating(function (Cart $cart) {
         //     $cart->id = Str::uuid();
         // });
@@ -35,5 +41,15 @@ class Cart extends Model
     public function product()
     {
         return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+    public static function getCookieId()
+    {
+        $cookie_id = Cookie::get('cart_id');
+        if (!$cookie_id) {
+            $cookie_id = Str::uuid();
+            Cookie::queue('cart_id', $cookie_id, 30 * 24 * 60);
+        }
+        return $cookie_id;
     }
 }
