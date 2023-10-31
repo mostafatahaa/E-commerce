5d797161ad90bf611c7aba953233a60ad8f0ee1b
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-31 02:35:17 +0200

    create cart observer

---
+++ app/Observers/CartObserver.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Cart;
+use Illuminate\Support\Str;
+
+class CartObserver
+{
+    /**
+     * Handle the Cart "creating" event.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return void
+     */
+    public function creating(Cart $cart)
+    {
+        $cart->id = Str::uuid();
+    }
+
+    /**
+     * Handle the Cart "updated" event.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return void
+     */
+    public function updated(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Handle the Cart "deleted" event.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return void
+     */
+    public function deleted(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Handle the Cart "restored" event.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return void
+     */
+    public function restored(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Handle the Cart "force deleted" event.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return void
+     */
+    public function forceDeleted(Cart $cart)
+    {
+        //
+    }
+}
