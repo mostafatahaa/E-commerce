ccc834365e06fb949c4a974a6ee73a8030edbef2
Mostafa Taha <mostafataha011188@gmail.com>
2023-11-02 19:28:32 +0200

    create table order_items

---
+++ database/migrations/2023_11_01_202540_create_order_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')
+                ->constrained('orders')
+                ->cascadeOnDelete();
+            $table->foreignId('product_id')->nullable()
+                ->constrained('products')
+                ->nullOnDelete();
+            $table->string('product_name');
+            $table->float('price');
+            $table->unsignedSmallInteger('quantity')->default(1);
+            $table->json('options')->nullable();
+
+            $table->unique(['order_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
