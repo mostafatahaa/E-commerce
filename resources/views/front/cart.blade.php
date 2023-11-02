8c7bd50127822084d3db45f0c676bb9cd4c7f0c3
Mostafa Taha <mostafataha011188@gmail.com>
2023-11-03 00:00:38 +0200

    add route to button checkout

--- resources/views/front/cart.blade.php
+++ resources/views/front/cart.blade.php
@@ -111,7 +111,7 @@
                                         <li class="last">You Pay<span>$2531.00</span></li>
                                     </ul>
                                     <div class="button">
-                                        <a href="checkout.html" class="btn">Checkout</a>
+                                        <a href="{{route('checkout')}}" class="btn">Checkout</a>
                                         <a href="product-grids.html" class="btn btn-alt">Continue shopping</a>
                                     </div>
                                 </div>
