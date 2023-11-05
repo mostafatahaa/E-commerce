6752e01a21bb1f867f4ebfa6866096bd4fc5135c
Mostafa Taha <mostafataha011188@gmail.com>
2023-11-05 03:53:01 +0200

    add fortify service provider to providers

--- config/app.php
+++ config/app.php
@@ -193,6 +193,7 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class, // this is 
         App\Providers\AuthServiceProvider::class,
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
