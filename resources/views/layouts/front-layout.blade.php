565169fdade5d36bff3428aec5678d0967f095b7
Mostafa Taha <mostafataha011188@gmail.com>
2023-11-06 06:29:50 +0200

    set condition for shows username in navbar

--- resources/views/layouts/front-layout.blade.php
+++ resources/views/layouts/front-layout.blade.php
@@ -87,18 +87,38 @@
                     </div>
                     <div class="col-lg-4 col-md-4 col-12">
                         <div class="top-end">
+
+                            @auth('web')
+                            <div class="user">
+                                <i class="lni lni-user"></i>
+                                {{Auth::guard('web')->user()->name}}
+                            </div>
+
+                            <ul class="user-login">
+                                <form action="{{route('logout')}}" method="post" id="logout" style="display:none;">
+                                    @csrf
+                                </form>
+                                <li>
+                                    <a href="{{route('logout')}}" onclick="event.preventDefault(); getElementById('logout').submit()">Sign Out</a>
+                                </li>
+
+                            </ul>
+
+                            @else
                             <div class="user">
                                 <i class="lni lni-user"></i>
                                 Hello
                             </div>
                             <ul class="user-login">
                                 <li>
-                                    <a href="login.html">Sign In</a>
+                                    <a href="{{route('login')}}">Sign In</a>
                                 </li>
                                 <li>
-                                    <a href="register.html">Register</a>
+                                    <a href="{{route('register')}}">Register</a>
                                 </li>
                             </ul>
+                            @endauth
+
                         </div>
                     </div>
                 </div>
