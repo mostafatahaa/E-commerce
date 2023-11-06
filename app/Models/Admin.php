cd4f1bb5de704b4985451ea7f2048cb1be16db5a
Mostafa Taha <mostafataha011188@gmail.com>
2023-11-06 06:25:37 +0200

    create admin model

---
+++ app/Models/Admin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends User
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'phone_number', 'super_admin', 'status'
+    ];
+}
