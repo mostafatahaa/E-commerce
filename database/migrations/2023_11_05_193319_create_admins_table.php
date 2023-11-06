4e7698c3a1be0f1fa8554d4d8271654dd57eb980
Mostafa Taha <mostafataha011188@gmail.com>
2023-11-06 06:27:52 +0200

    create table admins

--- database/migrations/2023_11_05_193319_create_admins_table.php
+++ database/migrations/2023_11_05_193319_create_admins_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email');
             $table->string('username')->unique();
             $table->string('password');
             $table->string('phone_number')->unique();
