f072dca79637d183f234590b972dcf796bba4367
Mostafa Taha <mostafataha011188@gmail.com>
2023-11-07 03:21:34 +0200

    remove breadcrumb from login page..no needed

--- resources/views/front/auth/login.blade.php
+++ resources/views/front/auth/login.blade.php
@@ -1,29 +1,4 @@
 <x-front-layout title="Lgoin">
-    <!-- Start Breadcrumbs -->
-    <x-slot:breadcrumb>
-        <div class="breadcrumbs">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 col-md-6 col-12">
-                        <div class="breadcrumbs-content">
-                            <h1 class="page-title">Login</h1>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-12">
-                        <ul class="breadcrumb-nav">
-                            <li><a href="{{route('home')}}"><i class="lni lni-home"></i> Home</a></li>
-                            <li>Login</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </x-slot:breadcrumb>
-    <!-- End Breadcrumbs -->
-
-
-    <!-- End Header Area -->
-
     <!-- Start Account Login Area -->
     <div class="account-login section">
         <div class="container">
