daad09b2b2cd6ee96307bd07e664b1d5cca3eeb6
Mostafa Taha <mostafataha011188@gmail.com>
2023-11-08 03:57:00 +0200

    create api resource class for product

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'    => $this->id,
+            'product_name'  => $this->name,
+            'price' => [
+                'normal' => $this->price,
+                'compare' => $this->compare_price,
+            ],
+
+            'description' => $this->description,
+            'status' => $this->status,
+            'image' => $this->image_url,
+
+            'relations' => [
+                'category' => [
+                    'id' => $this->category->id,
+                    'name' => $this->category->name,
+                ],
+                'store' => [
+                    'id' => $this->store->id,
+                    'name' => $this->store->name,
+                ],
+
+            ],
+        ];
+    }
+}
